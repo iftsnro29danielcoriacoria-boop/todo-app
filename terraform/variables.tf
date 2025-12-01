4909750a01824d15c669769f8f500365fd1968e1
santysanty <dsanty0@gmail.com>
2025-12-01 15:49:17 -0300

    Corrijo CI/CD: Agrego variables de secreto para Docker Hub y limpio historial

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,21 @@
-variable "mongo_uri" {
-  description = "MongoDB connection string"
+# Archivo: variables.tf
+
+# -----------------------------------------------------------
+# Variables de Docker Hub
+# -----------------------------------------------------------
+variable "docker_username" {
+  description = "Tu nombre de usuario de Docker Hub (danielcoria)"
   type        = string
+  default     = "danielcoria" # Aseguramos que sea 'danielcoria'
+}
+
+# -----------------------------------------------------------
+# Variables de la Aplicación
+# -----------------------------------------------------------
+variable "mongo_uri" {
+  description = "URI de conexión a la base de datos MongoDB. Contiene credenciales sensibles."
+  type        = string
+  # Ya tienes el valor correcto. Se añade 'sensitive = true' para ocultarlo en los logs de plan/apply.
+  default     = "mongodb+srv://Fede_22k:fede2014_22k@cluster0.dtk1apu.mongodb.net/ToDoList?retryWrites=true&w=majority&appName=Cluster0"
+  sensitive   = true 
 }
